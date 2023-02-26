8cc4d889e12f56ed9784172d9b4faba365800e7a
DianVK <diankinanev@abv.bg>
2023-02-26 20:06:10 +0200

    Solved task "Theatre Promotions"

---
+++ JS Syntax Fundamentals - Lab/theatre_promotions.js
@@ -0,0 +1,42 @@
+function theatrePromotions(type_of_day, age){
+
+    if (type_of_day === "Weekday") {
+        if (0 <= age && age <= 18){
+            console.log("12$")
+        } else if (18 <= age && age <= 64){
+            console.log("18$")
+        } else if (64 <= age && age <= 122){
+            console.log("12$")
+        } else if (0 > age || age > 122){
+            console.log("Error!")
+        }
+
+    } else if (type_of_day === "Weekend"){
+        if (0 <= age && age <= 18){
+            console.log("15$")
+        } else if (18 <= age && age <= 64){
+            console.log("20$")
+        } else if (64 <= age && age <= 122){
+            console.log("15$")
+        } else if (0 > age || age > 122){
+            console.log("Error!")
+        }
+
+    } else if (type_of_day === "Holiday"){
+        if (0 <= age && age <= 18){
+            console.log("5$")
+        } else if (18 <= age && age <= 64){
+            console.log("12$")
+        } else if (64 <= age && age <= 122){
+            console.log("10$")
+        } else if (0 > age || age > 122){
+            console.log("Error!")
+        }
+    }
+}
+
+theatrePromotions('Weekday', 42)
+
+theatrePromotions('Holiday', -12)
+
+theatrePromotions('Holiday', 15)
