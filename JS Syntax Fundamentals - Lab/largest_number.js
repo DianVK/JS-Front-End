1024646c8714c1fa57d246a3920f62fb505200c9
DianVK <diankinanev@abv.bg>
2023-02-26 19:45:46 +0200

    Solve task "Largest Number"

---
+++ JS Syntax Fundamentals - Lab/largest_number.js
@@ -0,0 +1,13 @@
+function findLargestNum(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3){
+        console.log(`The largest number is ${num1}.`)
+    } else if (num2 > num1 && num2 > num3) {
+        console.log(`The largest number is ${num2}.`)
+    } else if (num3 > num1 && num3 > num2) {
+        console.log(`The largest number is ${num3}.`)
+    }
+}
+
+findLargestNum(5, -3, 16)
+
+findLargestNum(-3, -5, -22.5)
