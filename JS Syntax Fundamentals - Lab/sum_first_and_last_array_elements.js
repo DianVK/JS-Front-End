25db5630d5500f1fd8aae3ba8fc2b019c709fce0
DianVK <diankinanev@abv.bg>
2023-02-27 10:48:47 +0200

    Solved task "Sum First and Last Array Elements"

---
+++ JS Syntax Fundamentals - Lab/sum_first_and_last_array_elements.js
@@ -0,0 +1,7 @@
+function sumFirstLastElement(elements) {
+    firstElement = elements[0]
+    lastElement = elements[elements.length - 1]
+    console.log(firstElement + lastElement)
+}
+
+sumFirstLastElement([20, 30, 40])
