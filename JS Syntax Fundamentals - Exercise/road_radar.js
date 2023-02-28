e7892f8028bcd62092177ace291923b012cb2239
DianVK <diankinanev@abv.bg>
2023-02-28 14:48:34 +0200

    Add function checkStatus

--- JS Syntax Fundamentals - Exercise/road_radar.js
+++ JS Syntax Fundamentals - Exercise/road_radar.js
@@ -1,18 +1,23 @@
 function roadRadar(speed,typeRoad) {
+    function checkStatus(difference) {
+        if (difference <= 20) {
+            stats = "speeding"
+            return stats
+        } else if (difference <= 40) {
+            stats = "excessive speeding"
+            return stats
+        } else {
+            stats = "reckless driving"
+            return stats
+        }
+    }
     let speedLimit = 0
     if (typeRoad === "motorway") {
         speedLimit = 130
         if (speed > 130){
             let diff = speed - speedLimit
-            let status = ""
-            if (diff <= 20) {
-                status = "speeding"
-            } else if (diff <= 40) {
-                status = "excessive speeding"
-            } else {
-                status = "reckless driving"
-            }
-            console.log(`The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - {status}`)
+            let status = checkStatus(diff)
+            console.log(`The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`)
         } else {
             console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
         }
@@ -20,14 +25,7 @@ function roadRadar(speed,typeRoad) {
         speedLimit = 90
         if (speed > 90){
             let diff = speed - speedLimit
-            let status = ""
-            if (diff <= 20) {
-                status = "speeding"
-            } else if (diff <= 40) {
-                status = "excessive speeding"
-            } else {
-                status = "reckless driving"
-            }
+            let status = checkStatus(diff)
             console.log(`The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`)
         } else {
             console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
@@ -36,30 +34,16 @@ function roadRadar(speed,typeRoad) {
         speedLimit = 50
         if (speed > 50){
             let diff = speed - speedLimit
-            let status = ""
-            if (diff <= 20) {
-                status = "speeding"
-            } else if (diff <= 40) {
-                status = "excessive speeding"
-            } else {
-                status = "reckless driving"
-            }
+            let status = checkStatus(diff)
             console.log(`The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`)
         } else {
             console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
         }
-    }else if (typeRoad === "residential") {
+    } else if (typeRoad === "residential") {
         speedLimit = 20
         if (speed > 20){
             let diff = speed - speedLimit
-            let status = ""
-            if (diff <= 20) {
-                status = "speeding"
-            } else if (diff <= 40) {
-                status = "excessive speeding"
-            } else {
-                status = "reckless driving"
-            }
+            let status = checkStatus(diff)
             console.log(`The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`)
         } else {
             console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
