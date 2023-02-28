4ad2cc3523a9bdb46ea46bc8c7dfded4b9a196eb
DianVK <diankinanev@abv.bg>
2023-02-28 17:03:41 +0200

    Solved task

---
+++ JS Syntax Fundamentals - Exercise/cooking_by_numbers.js
@@ -0,0 +1,25 @@
+function cooking(number,...operations) {
+
+    for (i = 0; i < operations.length; i++) {
+
+        let currentOperation = operations[i]
+
+        if (currentOperation === "chop") {
+            number = number / 2
+        }  else if (currentOperation === "dice") {
+            number = Math.sqrt(number)
+        } else if (currentOperation === "spice") {
+            number = number + 1
+        } else if (currentOperation === "bake") {
+            number = number * 3
+        } else if (currentOperation === "fillet") {
+            number = number * 0.80
+        } 
+         console.log(number.toFixed(1))
+    }
+    
+}
+
+//cooking('32', 'chop', 'chop', 'chop', 'chop', 'chop')
+
+cooking('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
--- JS Syntax Fundamentals - Exercise/road_radar.js
+++ JS Syntax Fundamentals - Exercise/road_radar.js
@@ -1,5 +1,6 @@
 function roadRadar(speed,typeRoad) {
     function checkStatus(difference) {
+
         if (difference <= 20) {
             stats = "speeding"
             return stats
@@ -12,6 +13,7 @@ function roadRadar(speed,typeRoad) {
         }
     }
     let speedLimit = 0
+
     if (typeRoad === "motorway") {
         speedLimit = 130
         if (speed > 130){
@@ -22,6 +24,7 @@ function roadRadar(speed,typeRoad) {
             console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
         }
     } else if (typeRoad === "interstate") {
+
         speedLimit = 90
         if (speed > 90){
             let diff = speed - speedLimit
@@ -31,6 +34,7 @@ function roadRadar(speed,typeRoad) {
             console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
         }
     } else if (typeRoad === "city") {
+
         speedLimit = 50
         if (speed > 50){
             let diff = speed - speedLimit
@@ -40,6 +44,7 @@ function roadRadar(speed,typeRoad) {
             console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
         }
     } else if (typeRoad === "residential") {
+        
         speedLimit = 20
         if (speed > 20){
             let diff = speed - speedLimit
