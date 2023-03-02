cb53db99b8be9be2a41a4a27ab7a6d7032b5cc12
DianVK <diankinanev@abv.bg>
2023-03-02 21:03:28 +0200

    Resolve task

--- Functions and Statements/simple_calculator.js
+++ Functions and Statements/simple_calculator.js
@@ -1,14 +1,35 @@
 function simpleCalculator(numOne, numTwo, operator) {
-    
-    const calculator = (numOne, numTwo, operator) => ({
-      'multiply': numOne * numTwo,
-      'divide': numOne / numTwo,
-      'add': numOne + numTwo,
-      'subtract': numOne - numTwo,
-    })[operator];
-  
-    const result = calculator(numOne, numTwo, operator);
-    console.log(result);
+  let result;
+  switch (operator) {
+    case 'multiply':
+      result = numOne * numTwo;
+      break;
+    case 'divide':
+      result = numOne / numTwo;
+      break;
+    case 'add':
+      result = numOne + numTwo;
+      break;
+    case 'subtract':
+      result = numOne - numTwo;
+      break;
+    default:
+      console.log('Invalid operator');
   }
+  console.log(result)
+}
+
+simpleCalculator(5, 5, 'multiply');
+
+// function simpleCalculator(numOne, numTwo, operator) {
+    
+//     const calculator = (numOne, numTwo, operator) => ({
+//       'multiply': numOne * numTwo,
+//       'divide': numOne / numTwo,
+//       'add': numOne + numTwo,
+//       'subtract': numOne - numTwo,
+//     })[operator];
   
-  simpleCalculator(5, 5, 'multiply');
+//     const result = calculator(numOne, numTwo, operator);
+//     console.log(result);
+//   }
