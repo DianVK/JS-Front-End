002da3798363a4896c21cc40bfa01899b3b11579
DianVK <diankinanev@abv.bg>
2023-03-02 21:18:17 +0200

    Solved task "Sign Check"

---
+++ Functions and Statements/sign_check.js
@@ -0,0 +1,15 @@
+function resultCheck(...numbers){
+    let negativeNumbers = 0
+    for (const num of numbers){
+        if (num < 0){
+            negativeNumbers += 1
+        }
+    }
+    if (negativeNumbers === 0 || negativeNumbers === 2){
+        console.log('Positive')
+    } else {
+        console.log('Negative')
+    }
+}
+
+resultCheck(-6, -12, 14)
